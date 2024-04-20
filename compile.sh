1e467a7b3605da3a4cae8c3b9c5e3f63ab33dec4
yanji <1849068856@qq.com>
2024-04-20 20:26:50 +0800

    新加载loaDER

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "start comile boot 相关文件"
+cd ./boot
+nasm -I include/ -o mbr2.bin mbr2.S
+nasm -I include/ -o loader.bin loader.S
+sudo dd if=//home/chen/OS/boot/mbr2.bin of=//home/bochs/bochs/bin/hd60M.img bs=512 count=1 conv=notrunc
+sudo dd if=//home/chen/OS/boot/loader.bin of=//home/bochs/bochs/bin/hd60M.img bs=512 count=4 seek=2 conv=notrunc
+echo "comile finish"
