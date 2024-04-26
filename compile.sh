070706322ab5ced3069e9f543d78870c7a3c10cc
yanji <1849068856@qq.com>
2024-04-26 21:40:21 +0800

    终于能够成功运行

--- compile.sh
+++ compile.sh
@@ -5,5 +5,5 @@ cd ./boot
 nasm -I include/ -o mbr2.bin mbr2.S
 nasm -I include/ -o loader.bin loader.S
 sudo dd if=//home/chen/OS/boot/mbr2.bin of=//home/bochs/bochs/bin/hd60M.img bs=512 count=1 conv=notrunc
-sudo dd if=//home/chen/OS/boot/loader.bin of=//home/bochs/bochs/bin/hd60M.img bs=512 count=4 seek=2 conv=notrunc
+sudo dd if=//home/chen/OS/boot/loader.bin of=//home/bochs/bochs/bin/hd60M.img bs=512 count=3 seek=2 conv=notrunc
 echo "comile finish"
