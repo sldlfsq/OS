ef69613013a8d2293bedcd0e036da3efa4db800f
yanji <1849068856@qq.com>
2024-05-03 17:00:26 +0800

    添加打印数据的功能

--- compile.sh
+++ compile.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 echo "start comile boot 相关文件"
-cd ./boot
-nasm -I include/ -o mbr2.bin mbr2.S
-nasm -I include/ -o loader.bin loader.S
-sudo dd if=//home/chen/OS/boot/mbr2.bin of=//home/bochs/bochs/bin/hd60M.img bs=512 count=1 conv=notrunc
-sudo dd if=//home/chen/OS/boot/loader.bin of=//home/bochs/bochs/bin/hd60M.img bs=512 count=3 seek=2 conv=notrunc
 
-cd ../kernel
-gcc -m32 -c -o main.o main.c
-ld -m elf_i386 main.o -Ttext 0xc0001500 -e main -o kernel.bin
+# nasm -I boot/include/ -o boot/mbr2.bin boot/mbr2.S
+# nasm -I boot/include/ -o boot/loader.bin boot/loader.S
+
+nasm -f elf -o lib/kernel/print.o lib/kernel/print.S
+# sudo dd if=//home/chen/OS/boot/mbr2.bin of=//home/bochs/bochs/bin/hd60M.img bs=512 count=1 conv=notrunc
+# sudo dd if=//home/chen/OS/boot/loader.bin of=//home/bochs/bochs/bin/hd60M.img bs=512 count=3 seek=2 conv=notrunc
+
+gcc -m32 -I lib/kernel/ -c -o kernel/main.o kernel/main.c
+# ld -m elf_i386 main.o -Ttext 0xc0001500 -e main -o kernel.bin
+ld -m elf_i386 -Ttext 0xc0001500 -e main -o kernel/kernel.bin kernel/main.o lib/kernel/print.o
 sudo dd if=//home/chen/OS/kernel/kernel.bin of=//home/bochs/bochs/bin/hd60M.img bs=512 count=200 seek=9 conv=notrunc
 echo "comile finish"
--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,8 @@
+#include "print.h"
 int main()
 {
+    put_str("i am kernel\n");
+    put_int(0x12345678);
     while(1);
     return 0;
 }
